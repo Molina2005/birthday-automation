3a78f9402a920af32161ab2d4b074022ccc04216
Cristian Molina <cm1094871@gmail.com>
2025-10-02 20:48:28 -0500

    estructura inicial del proyecto

---
+++ structs/structs.go
@@ -0,0 +1,24 @@
+package structs
+
+import (
+	// "text/template"
+	"time"
+)
+
+type PlantillaCumpleanos struct {
+	Titulo           string
+	Fecha            int
+	Nombre           string
+	Apellido         string
+	Descripcion      string
+	Edad             int
+	Funcionarios     []string
+	Dias             int
+}
+
+type DatosFuncionarios struct {
+	FechaNacimiento     time.Time
+	NombreFuncionario   string
+	ApellidoFuncionario string
+	CorreoFuncionario   string
+}
